7252849a4549743c885c91fa2a2686bd7a70e8f2
louis <louiscahyadi23@gmail.com>
2020-03-16 19:18:08 +0700

    Teacher list on course page

---
+++ src/Components/TeacherList/TeacherList.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './TeacherList.css';
+import TeacherInCoursePage from './TeacherInCoursePage';
+
+function TeacherList() {
+    const [teacherList, setTeacherList] = useState(['Louis Cahyadi', 'Kevin Nathaniel Wijaya'])
+
+    return (
+        <div className="teacher-list">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Teachers</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { teacherList.map((teacher) => {
+                            return (<tr>
+                                <td><TeacherInCoursePage name={teacher} /></td>
+                            </tr>
+                    )})}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default TeacherList;
