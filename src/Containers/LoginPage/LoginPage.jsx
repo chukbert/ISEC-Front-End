c4dfb4ccafce21bd7a68477e4a09c975b43bb807
louis <louiscahyadi23@gmail.com>
2020-03-21 15:33:24 +0700

    View - login page

---
+++ src/Containers/LoginPage/LoginPage.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
+import './LoginPage.css';
+
+class LoginPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            permission: 0,
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChange(e) {
+        const value = e.target.value;
+        this.setState({
+            [e.target.name]: value
+        });
+    }
+
+    handleSubmit() {
+        const api = '';
+        axios.post(api, {
+            username: this.state.username,
+            password: this.state.password
+        }).then(() => {
+            // TBD
+        })
+    }
+
+    render() {
+        return (
+            <div className="login-page">
+                <Form>
+                    <Form.Group controlId="loginUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="username" onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Form.Group controlId="loginPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Log in
+                    </Button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import App from './App'
+import LoginPage from './Containers/LoginPage/LoginPage';
 import * as serviceWorker from './serviceWorker';
-import CoursePage from './Containers/CoursePage/CoursePage';
+// import CoursePage from './Containers/CoursePage/CoursePage';
 
-ReactDOM.render(<CoursePage />, document.getElementById('root'));
+ReactDOM.render(<LoginPage /> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
