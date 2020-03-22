179aecde0ac7eb1e7a60de5acc1404ccb64ee723
tasyald <13517141@std.stei.itb.ac.id>
2020-03-22 16:52:00 +0700

    Add teacher modal

---
+++ src/Components/AddTeacher/AddTeacher.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Modal, Form, Button } from 'react-bootstrap'
+import './AddTeacher.css';
+
+function AddTeacher(props) {
+    const [name, setName] = useState(''); 
+
+    const handleChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const addTeacherToCourse = () => {
+        // const api = process.env.REACT_APP_API_HOST + ''
+        // axios.post(api, {"name": name}).then(function() {
+        //     window.location.reload();
+        // })
+    }
+    
+    return (
+        <div className="add-teacher" onClick={e => e.stopPropagation()}>
+            <Modal {...props} centered>
+                <Modal.Header>
+                    <Modal.Title>
+                        Add Teacher
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="form-add-course">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" placeholder="Enter username" onChange={handleChange}/>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={props.onHide} variant="secondary">Cancel</Button>
+                    <Button onClick={addTeacherToCourse} variant="primary">Add</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+}
+
+export default AddCourse;
