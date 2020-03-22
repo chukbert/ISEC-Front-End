9e0b60bdd18fe88d01044bf497e1ac35ca111a33
louis <louiscahyadi23@gmail.com>
2020-03-22 18:13:39 +0700

    basic program page structure

---
+++ src/Containers/ProgramPage/ProgramPage.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Axios from 'axios';
+import Description from '../../Components/Description/Description';
+import TeacherList from '../../Components/TeacherList/TeacherList';
+import CourseLink from '../../Components/CourseLink/CourseLink';
+
+class ProgramPage extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            permission: 0,
+            id: props.programId,
+            name: '',
+            listOfCourse: [],
+            listOfIsShowCourse: [],
+        }
+    }
+
+    render() {
+        return (
+            <div className="program-page">
+                <div className="program-title">
+                    <h1>{this.state.name}</h1>
+                </div>
+
+                <div className="program-page-desc-teacher-list">
+                    <div className="course-list">
+                        <CourseLink name="Dasar Pemrograman" id = {1} />
+                    </div>
+                    <div className="desc-teacher">
+                        <Description data={this.state.programDesciption}/>
+                        <TeacherList />
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default ProgramPage;
