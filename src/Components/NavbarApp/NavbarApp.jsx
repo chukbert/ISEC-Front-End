6dd9e3f1b69cf3f96f803731c3ebdba0934644a1
louis <louiscahyadi23@gmail.com>
2020-03-27 04:09:34 +0700

    routing

--- src/Components/NavbarApp/NavbarApp.jsx
+++ src/Components/NavbarApp/NavbarApp.jsx
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
+import Cookies from 'js-cookie';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './NavbarApp.css';
+import ProgramPage from '../../Containers/ProgramPage/ProgramPage';
+import CoursePage from '../../Containers/CoursePage/CoursePage';
 
-function NavbarApp() {
+function NavbarApp(props) {
     // eslint-disable-next-line no-unused-vars
+    const logout = () => {
+        Cookies.remove('token');
+        window.location.href='/';
+    }
+
+    const toProgram = () => {
+        window.location.href = '/programs';
+    }
+    
     return (
         <div className="navbar">
-            <Navbar fixed="top" expand="lg">
-                <div className="app-name">
-                    <Navbar.Brand href="#">ISEC</Navbar.Brand>
-                </div>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#">Courses</Nav.Link>
-                    <Nav.Link href="#">Programs</Nav.Link>
-                </Nav>
-                <NavDropdown title="User" id="nav-dropdown">
-                    <NavDropdown.Item href="#">Logout</NavDropdown.Item>
-                </NavDropdown>
-            </Navbar>
+            <Router>
+                <Navbar fixed="top" expand="lg">
+                    <div className="app-name">
+                        <Navbar.Brand href="/">ISEC</Navbar.Brand>
+                    </div>
+                    <Nav className="mr-auto">
+                        <Nav.Link>Courses</Nav.Link>
+                        <Nav.Link onClick={toProgram}>Programs</Nav.Link>
+                    </Nav>
+                    <NavDropdown title="User" id="nav-dropdown">
+                        <NavDropdown.Item href="/" onClick={logout}>Logout</NavDropdown.Item>
+                    </NavDropdown>
+                </Navbar>
+
+                <Switch>
+                    <Route path="/programs">
+                        <ProgramPage id="5e7cd0babe742b001dbfaf83" permission={props.permission}/>
+                    </Route>
+                    <Route path="/courses">
+                        <CoursePage permission={2} id={1} permission={props.permission}/>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
---
+++ src/Containers/Home/Home.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import Cookies from 'js-cookie';
+
+import ProgramPage from '../ProgramPage/ProgramPage';
+import CoursePage from '../CoursePage/CoursePage';
+
+class Home extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            permission: props.permission,
+        }
+        this.logout = this.logout.bind(this)
+    }
+
+    logout() {
+        Cookies.remove('token');
+        window.location.href='/';
+    }
+
+    render() {
+        return(
+            <div className="App">
+                <Router>
+                    <Navbar fixed="top" expand="lg">
+                        <div className="app-name">
+                            <Navbar.Brand href="/">ISEC</Navbar.Brand>
+                        </div>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/courses">Courses</Nav.Link>
+                            <Nav.Link href="/programs">Programs</Nav.Link>
+                        </Nav>
+                        <NavDropdown title="User" id="nav-dropdown">
+                            <NavDropdown.Item href="/" onClick={this.logout}>Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Navbar>
+
+                    <Switch>
+                        {/* <Route path="/">
+                            <h1>HOME</h1>
+                        </Route> */}
+                        {/* <Route path="/programs">
+                            <ProgramPage id="5e7cd0babe742b001dbfaf83" permission={this.state.permission}/>
+                        </Route> */}
+                        <Route path="/enrollprograms/:program_id" component={ProgramPage} />
+                        <Route path="/courses/:program_id/:course_id" component={CoursePage}/>
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
+}
+
+export default Home;
